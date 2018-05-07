7e8b65394d6d6f48fa2468d87ea012f8f3198ab8
codarsan <37899107+codarsan@users.noreply.github.com>
2018-05-07 11:31:16 +0100

    Add files via upload

---
+++ src/main/java/csan/springframework/WebDebugAppApplication.java
@@ -0,0 +1,12 @@
+package csan.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDebugAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDebugAppApplication.class, args);
+	}
+}
---
+++ src/test/java/csan/springframework/WebDebugAppApplicationTests.java
@@ -0,0 +1,16 @@
+package csan.springframework;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebDebugAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
