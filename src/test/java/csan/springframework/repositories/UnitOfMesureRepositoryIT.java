2da85edee534abc2571367cf59facc25d904c2b9
codarsan <@eclipse>
2018-06-09 14:48:46 +0100

    added failsafe plugin

---
+++ src/test/java/csan/springframework/repositories/UnitOfMesureRepositoryIT.java
@@ -0,0 +1,36 @@
+package csan.springframework.repositories;
+
+import static org.junit.Assert.*;
+import java.util.Optional;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import csan.springframework.model.UnitOfMesure;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMesureRepositoryIT {
+
+	@Autowired
+	UnitOfMesureRepository unitOfMesureRepository;
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testFindByUom() {
+		Optional<UnitOfMesure> findByUom = unitOfMesureRepository.findByUom("Tablespoon");
+		assertEquals("Tablespoon", findByUom.get().getUom());
+	}
+
+	@Test
+	public void testFindByUomCup() {
+		Optional<UnitOfMesure> findByUom = unitOfMesureRepository.findByUom("Cup");
+		assertEquals("Cup", findByUom.get().getUom());
+	}
+}
